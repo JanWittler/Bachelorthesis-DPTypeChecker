950efcb4b224785c03e399ac15b97bb53fd240bc
Jan Wittler <jan.wittler@innomos.com>
2017-06-07 12:08:26 +0200

    initial commit

---
+++ CGrammar/CGrammar.h
@@ -0,0 +1,19 @@
+//
+//  CGrammar.h
+//  CGrammar
+//
+//  Created by Jan Wittler on 04.06.17.
+//  Copyright © 2017 Jan Wittler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CGrammar.
+FOUNDATION_EXPORT double CGrammarVersionNumber;
+
+//! Project version string for CGrammar.
+FOUNDATION_EXPORT const unsigned char CGrammarVersionString[];
+
+#import <CGrammar/Absyn.h>
+#import <CGrammar/Parser.h>
+#import <CGrammar/Printer.h>
--- DPTypeChecker/main.swift
+++ DPTypeChecker/main.swift
@@ -1,12 +1,29 @@
 //
 //  main.swift
-//  DPTypeChecker
+//  BNFC_cToSwift
 //
-//  Created by Jan Wittler on 07.06.17.
+//  Created by Jan Wittler on 04.06.17.
 //  Copyright © 2017 Jan Wittler. All rights reserved.
 //
 
 import Foundation
+import CGrammar
 
-print("Hello, World!")
+let parse_tree: CGrammar.Program?
+if CommandLine.argc > 1, let file = fopen(CommandLine.arguments[1], "r") {
+    parse_tree = pProgram(file)
+}
+else {
+    parse_tree = pProgram(stdin)
+}
 
+guard let parse_tree = parse_tree else {
+    exit(1)
+}
+
+let swiftTree = CGrammarToSwiftBridge().visitProgram(parse_tree)
+print("Parse Successful!")
+print("\n[Abstract Syntax]")
+print(swiftTree.show())
+print("\n[Linearized Tree]")
+print(Swift.String(cString: printProgram(parse_tree)))
