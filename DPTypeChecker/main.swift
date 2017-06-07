d269f8a59dc5d2813844de6f8c1a3782dfd7b41d
Jan Wittler <jan.wittler@innomos.com>
2017-06-07 11:44:59 +0200

    Initial Commit

---
+++ DPTypeChecker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DPTypeChecker
+//
+//  Created by Jan Wittler on 07.06.17.
+//  Copyright © 2017 Jan Wittler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
