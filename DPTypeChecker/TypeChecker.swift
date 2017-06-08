a0a116950abb923514463fd3651ce321907a955d
Jan Wittler <jan.wittler@innomos.com>
2017-06-08 21:53:25 +0200

    implemented type checker to work with Example4.dpp

---
+++ DPTypeChecker/TypeChecker.swift
@@ -0,0 +1,184 @@
+//
+//  TypeChecker.swift
+//  DPTypeChecker
+//
+//  Created by Jan Wittler on 08.06.17.
+//  Copyright © 2017 Jan Wittler. All rights reserved.
+//
+
+import Foundation
+
+enum TypeCheckerError: Error {
+    case variableAlreadyExists(Id)
+    case variableNotFound(Id)
+    case invalidVariableAccess(Id)
+    case assertionFailed(String)
+    case other(String)
+}
+
+private struct Environment {
+    //there is always the global context
+    var contexts : [Context] = [Context()]
+    var currentContext: Context! {
+        get {
+            return contexts.last
+        }
+        set {
+            contexts.removeLast()
+            contexts.append(newValue)
+        }
+    }
+    
+    mutating func pushContext() {
+        contexts.append(Context())
+    }
+    
+    mutating func popContext() {
+        contexts.removeLast()
+    }
+    
+    func lookup(_ id: Id) throws -> Type {
+        //check topmost context first
+        for context in contexts.reversed() {
+            do {
+                return try context.lookup(id)
+            }
+            catch TypeCheckerError.variableNotFound(_) {
+                //error intended to occur, continue searching in next context
+            }
+        }
+        throw TypeCheckerError.variableNotFound(id)
+    }
+    
+    mutating func updateReplicationCount(for id: Id, delta: Double) throws {
+        var index = contexts.count - 1
+        while index >= 0 {
+            do {
+                var context = contexts[index]
+                try context.updateReplicationCount(for: id, delta: delta)
+                contexts[index] = context
+                return
+            }
+            catch TypeCheckerError.variableNotFound(_) {
+                //error intended to occur, continue searching in next context
+            }
+            index -= 1
+        }
+        throw TypeCheckerError.variableNotFound(id)
+    }
+}
+
+private struct Context {
+    private var values : [Id : Type] = [:]
+    
+    subscript(_ id: Id) -> Type? {
+        return values[id]
+    }
+    
+    mutating func add(_ id: Id, type: Type) throws {
+        guard !values.keys.contains(id) else {
+            throw TypeCheckerError.variableAlreadyExists(id)
+        }
+        guard type.replicationCount > 0 else {
+            throw TypeCheckerError.other("variable `\(id.value)` must be initialized with a nonnegative replication count")
+        }
+        values[id] = type
+    }
+    
+    mutating func updateReplicationCount(for id: Id, delta: Double) throws {
+        let type = try lookup(id)
+        let newRCount = type.replicationCount - delta
+        guard newRCount >= 0 else {
+            throw TypeCheckerError.invalidVariableAccess(id)
+        }
+        let newType = Type.tType(type.internalType, newRCount)
+        values[id] = newType
+    }
+    
+    func lookup(_ id: Id) throws -> Type {
+        if let type = values[id] {
+            return type
+        }
+        throw TypeCheckerError.variableNotFound(id)
+    }
+}
+
+private var environment = Environment()
+
+func typeCheck(_ program: Program) throws {
+    environment = Environment()
+    switch program {
+    case let .pDefs(stms):
+        try stms.forEach { try checkStm($0) }
+    }
+}
+
+private func checkStm(_ stm: Stm) throws {
+    switch stm {
+    case let .sInit(id, exp):
+        try environment.currentContext.add(id, type: inferType(exp))
+    case let .sInitExplicitType(id, type, exp):
+        //TODO: uncomment this when subtyping is implemented
+        let _ = exp //TODO: remove when subtyping is implemented, used to remove warning
+//        let expType = try inferType(exp)
+//        guard type == expType || type.isSubtype(expType) else {
+//            throw TypeCheckerError.other("type of assignment does not match expression" + "\n" +
+//                "exp: " + exp.show() + "\n" +
+//                "expected type: " + type + "\n" +
+//                "actual type: " + expType)
+//        }
+        try environment.currentContext.add(id, type: type)
+        
+    case let .sAssert(assertion):
+        try checkAssertion(assertion)
+    }
+}
+
+private func inferType(_ exp: Exp) throws -> Type {
+    switch exp {
+    case .eInt(_):
+        return .tType(.iTBase(.int), .infinity)
+    case let .eId(id):
+        try environment.updateReplicationCount(for: id, delta: 1)
+        return try environment.lookup(id)
+    case let .ePair(e1, e2):
+        let type1 = try inferType(e1)
+        let type2 = try inferType(e2)
+        return .tType(.iTMulPair(.tType(type1.internalType, 1), .tType(type2.internalType, 1)), 1)
+    case let .eTyped(_, type):
+        //TODO: for now this is accepted to get types where inferencing does not work yet but should be removed as soon as possible
+        return type
+    }
+}
+
+private func checkAssertion(_ assertion: Assertion) throws {
+    switch assertion {
+    case let .aTypeEqual(id, type):
+        let idType = try environment.lookup(id)
+        guard idType == type else {
+            let errorMessage = "variable `\(id.value)` does not match type\n" +
+                "expected: \(type)\n" +
+                "actual: \(idType)"
+            throw TypeCheckerError.assertionFailed(errorMessage)
+        }
+    }
+}
+
+//MARK: TypeCheckerError printing
+
+extension TypeCheckerError: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case let .variableAlreadyExists(id):
+            return "variable `\(id.value)` already exists in context"
+        case let .variableNotFound(id):
+            return "variable `\(id.value)` not found in context"
+        case let .invalidVariableAccess(id):
+            return "access to variable `\(id.value)` led to replication count less than zero"
+        case let .assertionFailed(message):
+            return message
+        case let .other(message):
+            return message
+        }
+    }
+}
