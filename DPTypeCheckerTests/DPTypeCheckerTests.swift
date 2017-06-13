8a93a1d9a72340d403c73ab62ab0bc15e863f897
Jan Wittler <jan.wittler@innomos.com>
2017-06-13 11:45:01 +0200

    added test scheme

---
+++ DPTypeCheckerTests/DPTypeCheckerTests.swift
@@ -0,0 +1,22 @@
+//
+//  DPTypeCheckerTests.swift
+//  DPTypeCheckerTests
+//
+//  Created by Jan Wittler on 13.06.17.
+//  Copyright © 2017 Jan Wittler. All rights reserved.
+//
+
+import XCTest
+
+class DPTypeCheckerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
